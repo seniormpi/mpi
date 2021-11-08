d48b3344a64e9a65f2bb96065660349fdd1ee075
Bulut Gözübüyük <bulutgozubuyuk@gmail.com>
2021-11-08 21:08:10 +0300

    import scripts

---
+++ importlar.bat
@@ -0,0 +1,7 @@
+pip install vtkplotlib
+pip install numpy
+pip install pathlib2
+pip install matplotlib
+pip install vtk
+pip install future
+pip install pyside6
--- importlar.sh
+++ importlar.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 pip install vtkplotlib
 pip install numpy
 pip install pathlib2
 pip install matplotlib
 pip install vtk
 pip install future
+pip install pyside6
