8f4766c9817c276a19520502814da8c50411e384
irmak Akyeli <45370715+irmakakyeli@users.noreply.github.com>
2021-12-01 12:24:23 +0300

    predict function

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
+import random
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QVBoxLayout
 import numpy as np
+import pandas as pd
 import binvox_rw
 import vtkplotlib as vpl
 from stl.mesh import Mesh
@@ -9,6 +11,18 @@ import tkinter as tk
 from tkinter import filedialog
 import subprocess
 from shutil import copyfile
+import os
+import numpy as np
+import tensorflow
+import keras
+from keras import regularizers
+import os
+import random
+from keras.layers import Input, MaxPooling3D, MaxPooling2D, Dense, Flatten, BatchNormalization, Dropout
+from keras.layers.convolutional import Conv3D
+from keras.models import Model
+from keras import optimizers
+from keras import regularizers
 
 copied_stl_path = ".\\out\\input.stl"
 
@@ -78,7 +92,7 @@ class MyWidget(QtWidgets.QWidget):
         subprocess.call('rmdir /q /s out', shell=True)
         subprocess.call('mkdir out', shell=True)
         copyfile(stl_path, copied_stl_path)
-        subprocess.call(['.\\executables\\binvox.exe', '-c', '-d', '100', copied_stl_path])
+        subprocess.call(['.\\executables\\binvox.exe', '-c', '-d', '64', copied_stl_path])
 
         # enable next step buttons
         self.view_binvox_btn.setEnabled(True)
@@ -104,8 +118,36 @@ class MyWidget(QtWidgets.QWidget):
     # predict
     @QtCore.Slot()
     def predict_out(self):
-        print("will predict")
-
+        
+        #predict of real parts with model 2
+        model = keras.models.Sequential()
+        model.add(Conv3D(32, 7, strides=2, padding='valid', activation='relu', input_shape = (64,64,64,1)))
+        model.add(Conv3D(32, 5, strides=1, padding='same', activation='relu'))
+        model.add(Conv3D(64, 3, strides=1, padding='same', activation='relu'))
+        model.add(Conv3D(64, 3, strides=1, padding='same', activation='relu'))
+        model.add(MaxPooling3D(pool_size=(2, 2, 2), padding='same'))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu',kernel_regularizer=regularizers.l2(0.0)))
+        model.add(Dense(16, activation='relu',kernel_regularizer=regularizers.l2(0.0)))
+        model.add(Dense(2, activation = 'sigmoid'))
+        model.load_weights('models/model_mpi.h5')
+        
+        batch_input = []
+        with open("./out/input.binvox", 'rb') as file:
+            try:
+                data = np.int32(binvox_rw.read_as_3d_array(file).data)
+            except:
+                print(file)
+        model_input = np.reshape(data, (64, 64, 64, 1))
+        batch_input += [model_input]
+        batch_x = np.array(batch_input)
+        
+        result = model.predict(batch_x)
+        result[:,0][result[:,0] >= 0.5] = 1
+        result[:,1][result[:,1] >= 0.5] = 1
+        result[:,1][result[:,1] < 0.5] = 0
+        result[:,0][result[:,0] < 0.5] = 0
+        print("Classification Results", result)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, widget):
