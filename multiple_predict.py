b910ef2396e463ec5e72c8e7798fca8542c0cb2d
irmak Akyeli <45370715+irmakakyeli@users.noreply.github.com>
2022-03-27 20:20:43 +0300

    confusion matrix

--- multiple_predict.py
+++ multiple_predict.py
@@ -7,6 +7,10 @@ import tensorflow
 from keras.layers import MaxPooling3D, Dense, Flatten
 from keras.layers.convolutional import Conv3D
 from keras import regularizers
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -35,6 +39,11 @@ model2.add(Dense(16, activation='relu', kernel_regularizer=regularizers.l2(0.0))
 model2.add(Dense(1, activation='sigmoid'))
 model2.load_weights('models/model_classify_mach_non_mach.h5')
 
+isMach =  []
+howMach = []
+y_true = []
+y_true2 = [1, 2, 3, 1, 1, 1, 2, 3, 2 ,3 ,1 ,2 ,3 , 2, 1, 1 ,2 ,1, 1, 2 ,3, 1, 2, 1, 1, 1, 2, 2, 2, 2, 1, 2, 1, 2, 3, 1, 2, 3, 1, 2, 2, 2, 2, 1 , 2 , 1, 1, 1]
+
 for filename in os.listdir('deneme'):
         batch_input = []
         with open('./deneme/' + filename, 'rb') as file:
@@ -46,17 +55,17 @@ for filename in os.listdir('deneme'):
         model_input = np.reshape(data, (64, 64, 64, 1))
         batch_input = [model_input]
         batch_x = np.array(batch_input)
-
-        result2 = model2.predict(batch_x)
+        y_true.append(1)
+        
+        result2 = model2.predict_generator(batch_x)
         res2 = ""
         if (result2[:][result2[:] < 0.5]):
             res2 = "not machinable"
+            isMach.append(0)
         if (result2[:][result2[:] >= 0.5]):
             res2 = "machinable"
-
-        """if (res2 == "not machinable"):
-            print(filename, " " , res2)
-        else: """   
+            isMach.append(1)
+          
         batch_input2 = []
         with open('./deneme/' + filename, 'rb') as file:
             try:
@@ -66,14 +75,48 @@ for filename in os.listdir('deneme'):
         model_input = np.reshape(data, (64, 64, 64, 1))
         batch_input2 = [model_input]
         batch_x = np.array(batch_input2)
-        result = model.predict(batch_x)
+        result = model.predict_generator(batch_x)
         res = ""
+        count = 0
         if (result[:, 0][result[:, 0] >= 0.5]):  # = 1
             res += "Milling "
+            count = 1
         if (result[:, 1][result[:, 1] >= 0.5]):  # = 1
             res += "Turning "
+            if(count == 1):
+                count = 3
+            else:
+                count = 2   
         if (result[:, 1][result[:, 1] < 0.5]):  # = 0
             res += " "
         if (result[:, 0][result[:, 0] < 0.5]):  # = 0
             res += " "
-        print(filename, " " , res2 , "procidure: ", res)
+        howMach.append(count)
+        #print(filename, " " , res2 , "procidure: ", res)
+
+
+# Define by steps in predict_generator * batch_size
+
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+cm = confusion_matrix(isMach, y_true)
+np.savetxt('part1_cm_feature.txt', cm)
+cmn = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+lbl = ["Mach", "non-mach"]
+df_cm = pd.DataFrame(cm, index=[i for i in lbl],columns=[i for i in lbl])
+plt.figure(figsize=(10, 7))
+cn_heat = sns.heatmap(df_cm, annot=True, cmap='Blues')
+fig = cn_heat.get_figure()
+fig.savefig('Mach_nonMach.png')
+
+
+cm = confusion_matrix(howMach, y_true2)
+np.savetxt('part1_cm_feature.txt', cm)
+cmn = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+lbl = ["Milling", "Turning", "Milling-Turning"]
+df_cm = pd.DataFrame(cm, index=[i for i in lbl],columns=[i for i in lbl])
+plt.figure(figsize=(10, 7))
+cn_heat = sns.heatmap(df_cm, annot=True, cmap='Blues')
+fig = cn_heat.get_figure()
+fig.savefig('howMach.png')
